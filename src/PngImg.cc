72764871f6b9034d84d9bd5101787f2742ced28d
Anton Usmansky <cody0@yandex-team.ru>
2015-05-05 16:57:42 +0300

    Check each argument separately for negativeness

--- src/PngImg.cc
+++ src/PngImg.cc
@@ -142,6 +142,10 @@ bool PngImg::InBounds_(png_uint_32 offsetX, png_uint_32 offsetY, png_uint_32 wid
 {
     return width != 0
         && height != 0
+        && width <= info_.width
+        && height <= info_.height
+        && offsetX < info_.width
+        && offsetY < info_.height
         && offsetX + width <= info_.width
         && offsetY + height <= info_.height;
 }
