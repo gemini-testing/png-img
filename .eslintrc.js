979bbcbd9eacbf7459b68ca5c73a3e4cf1b8d884
oldskytree <oldskytree@yandex-team.ru>
2022-09-04 23:19:16 +0300

    build: add linters for ts files

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    extends: ['gemini-testing', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: true
+};
